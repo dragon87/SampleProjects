0333f848061e2f0b283c501c2c96d3f3f9c59d9f
DH <dragos.haidet@gmail.com>
2018-05-12 23:17:54 +0300

    Added main character

---
+++ EscapeGame.Tests/PlayerUnitTests.cs
@@ -0,0 +1,24 @@
+﻿using EscapeGameConsole.GameElements;
+using Xunit;
+
+namespace EscapeGame.Tests
+{
+	public class PlayerUnitTests
+    {
+		[Fact]
+        public void PlayerVisitsEmptyCellLifeSoars()
+        {
+			//Given a player
+			Player player = new Player(null);
+			int expectedLife = 11;
+
+			//When it visits an empty cell
+			player.Visit(new EmptyCell());
+
+			//Then its life soars by 1
+			Assert.Equal(expectedLife, player.Life);
+        }
+
+
+    }
+}
---
+++ EscapeGameConsole/GameElements/Player.cs
@@ -0,0 +1,61 @@
+﻿namespace EscapeGameConsole.GameElements
+{
+	public class Player : Combatant
+	{
+		GameElement _host;
+
+		int _toxicPotionCount;
+
+		public Player(GameElement host, int initialLife = 10) 
+			: base(initialLife)
+		{
+			_host = host;
+		}
+
+		public void Visit(GameElement gameElement)
+		{
+			Visit((dynamic)gameElement);
+
+			if (Life > 0)
+			{
+				gameElement.MarkAsConsumed();
+			}
+		}
+
+		private void Visit(EmptyCell emptyCell)
+		{
+			Life += emptyCell.Life;
+		}
+        
+		private void Visit(ToxicPotion toxicPotion)
+        {
+			_toxicPotionCount += toxicPotion.Life;
+        }
+
+		private void Visit(HealingPotion healingPotion)
+		{
+			if (Life > healingPotion.Life)
+			{
+				return;
+			}
+
+			Life = healingPotion.Life;
+		}
+
+		private void Visit(Monster monster)
+		{
+			if (_toxicPotionCount > 0)
+			{
+				_toxicPotionCount--;
+				return;
+			}
+            
+			//TODO: Call FightManager to resolve the dispute
+		}
+
+		private void Visit(BigMonster bigMonster)
+		{
+			//TODO: Call FightManager to resolve the dispute
+		}
+	}
+}
