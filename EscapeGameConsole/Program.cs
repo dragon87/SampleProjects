29f99fc1a11f316c77a7f99876e1f99d664a77e0
DH <dragos.haidet@gmail.com>
2018-05-12 21:33:33 +0300

    Initial commit

---
+++ EscapeGameConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EscapeGameConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
